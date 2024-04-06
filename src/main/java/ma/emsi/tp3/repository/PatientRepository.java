8241fa47d5c08cf991837666f95cc4f5b70de164
zentaryassine <zentaryassine19@gmail.com>
2024-04-06 10:41:50 +0000

    first commit

---
+++ src/main/java/ma/emsi/tp3/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package ma.emsi.tp3.repository;
+
+import com.example.new_hospital.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
+}
